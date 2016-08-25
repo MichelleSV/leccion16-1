33a70404c1fcf61f54d4cd711b4b639c3f30b0fe
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-08-25 14:24:07 -0500

    correccion de detalles y poniendo el titulo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 var num1 = parseInt(prompt("Escribe un número"));
 var num2 = parseInt(prompt("Escribe un número"));
-document.write(num1 + num2);
+document.write("Si sumamos los dos números que ingresaste sería: " + (num1 + num2));
